69a9decfb17121a699cea783fb3ae1b66a283c6c
Akonaay <ambrosesulley4@gmail.com>
2021-04-27 10:17:52 +0300

    Index update

--- assets/js/app.js
+++ assets/js/app.js
@@ -57,4 +57,21 @@ if (jQuery().quicksand) {
 	
 }//if quicksand
 
+	// Porfolio - uses the magnific popup jQuery plugin
+  $('.portfolio-popup').magnificPopup({
+    type: 'image',
+    removalDelay: 300,
+    mainClass: 'mfp-fade',
+    gallery: {
+      enabled: true
+    },
+    zoom: {
+      enabled: true,
+      duration: 300,
+      easing: 'ease-in-out',
+      opener: function (openerElement) {
+        return openerElement.is('img') ? openerElement : openerElement.find('img');
+      }
+    }
+  });
 })
